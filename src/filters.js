88464b1ed43ea1cb90e1fddc94df1415e1c2ac82
Antares Tupin <antares.tupin@blablacar.com>
2016-10-29 18:07:18 +0200

    Added a way to execute js in templates

--- src/filters.js
+++ src/filters.js
@@ -3,58 +3,69 @@ const fs = require('fs')
 
 module.exports = (model, config) => {
     return {
-        flower: str => Array.isArray(str) ? str.map(i => flower(i)): flower(str),
-        fupper: str => Array.isArray(str) ? str.map(i => fupper(i)): fupper(str),
+        flower: flower,
+        fupper: flower,
 
-        snakecase: str => Array.isArray(str) ? str.map(i => splitVariableName(i).join('_')): splitVariableName(str).join('_'),
-        kebabcase: str => Array.isArray(str) ? str.map(i => splitVariableName(i).join('-')): splitVariableName(str).join('-'),
-        pascalcase: str => Array.isArray(str) ? str.map(i => pascalcase(i)): pascalcase(str),
-        camelcase: str => Array.isArray(str) ? str.map(i => flower(pascalcase(i))): flower(pascalcase(str)),
-        uppercamelcase: str => Array.isArray(str) ? str.map(i => pascalcase(i)): pascalcase(str),
-        lowercamelcase: str => Array.isArray(str) ? str.map(i => flower(pascalcase(i))): flower(pascalcase(str)),
+        snakecase: snakeCase,
+        kebabcase: kebabCase,
+        pascalcase: pascalCase,
+        camelcase: camelCase,
+        uppercamelcase: upperCamelCase,
+        lowercamelcase: lowerCamelCase,
 
-        prepend: (list, toPrepend) => list.map(str => toPrepend + str),
-        append: (list, toAppend) => list.map(str => str + toAppend),
-        keys: dict => Object.keys(dict),
-        values: dict => { let res = []; for (let i in dict) res.push(dict[i]); return res },
-        findByColumn: (list, column, value) => list.filter(i => i[column] == value),
-        findOneByColumn: (list, column, value) => list.filter(i => i[column] == value)[0],
+        prepend: prepend,
+        append: append,
+        keys: keys,
+        values: values,
+        findByColumn: findByColumn,
+        findOneByColumn: findOneByColumn,
+        map: map,
+        filter: filter,
 
-        json: dict => JSON.stringify(dict),
-        yaml: dict => yaml.safeDump(dict),
+        json: jsonDump,
+        yaml: yamlDump,
 
         findinmodel: str => findInModel(str, model, config),
-        findoneinmodel: str => findInModel(str, model, config)[0],
+        findoneinmodel: findOneInModel,
 
-        readfile: path => fs.readFileSync(path, 'utf8')
+        readfile: readFile,
+
+        apply: apply
     }
 }
 
 function splitVariableName(varName) {
-    // snake_case
-    let split = varName.split('_')
-    if (split.length > 1) return split
-
-    // kebab-case
-    split = varName.split('-')
-    if (split.length > 1) return split
-
-    // camelCase / PascalCase
-    split = varName.replace(/([a-z](?=[A-Z]))/g, '$1-').toLowerCase().split('-')
-    return split
+    return ['-', '_'].map(i => varName.split(i)).find(i => i.length > 1) // kebab-case / snake_case
+        || varName.replace(/([a-z](?=[A-Z]))/g, '$1-').toLowerCase().split('-') // PascalCase / camelCase
 }
 
-function pascalcase(str) {
-    return splitVariableName(str).map(i => fupper(i)).join('')
-}
+const snakeCase = str => Array.isArray(str) ? str.map(i => snakeCase(i)): splitVariableName(str).join('_')
+const kebabCase = str => Array.isArray(str) ? str.map(i => kebabCase(i)): splitVariableName(str).join('-')
+const camelCase = str => Array.isArray(str) ? str.map(i => camelCase(i)): flower(pascalCase(str))
+const pascalCase = str => Array.isArray(str) ? str.map(i => pascalCase(i)): splitVariableName(str).map(i => fupper(i)).join('')
+const upperCamelCase = str => pascalCase(str)
+const lowerCamelCase = str => camelCase(str)
 
-function flower(str) {
-    return str.charAt(0).toLowerCase() + str.substr(1)
-}
+const flower = str => Array.isArray(str) ? str.map(i => flower(i)): str.charAt(0).toLowerCase() + str.substr(1)
+const fupper = str => Array.isArray(str) ? str.map(i => fupper(i)): str.charAt(0).toUpperCase() + str.substr(1)
 
-function fupper(str) {
-    return str.charAt(0).toUpperCase() + str.substr(1)
-}
+const prepend = (list, toPrepend) => list.map(str => toPrepend + str)
+const append = (list, toAppend) => list.map(str => str + toAppend)
+const keys = dict => Object.keys(dict)
+const values = dict => { let res = []; for (let i in dict) res.push(dict[i]); return res }
+const findByColumn = (list, column, value) => list.filter(i => i[column] == value)
+const findOneByColumn = (list, column, value) => list.filter(i => i[column] == value)[0]
+const map = (collection, fn) => collection.map(eval(fn))
+const filter = (collection, fn) => collection.filter(eval(fn))
+
+const jsonDump = dict => JSON.stringify(dict)
+const yamlDump = dict => yaml.safeDump(dict)
+
+const findOneInModel = str => findInModel(str, model, config)[0]
+
+const readFile = path => fs.readFileSync(path, 'utf8')
+
+const apply = (val, fn) => eval(fn)(val)
 
 // entity{name: user}
 function findInModel(request, model, config) {
