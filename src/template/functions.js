2cd52eb4d8b2bace34bdf7c869c5911d2e61c0e9
Antares Tupin <antares.tupin@blablacar.com>
2016-11-08 20:18:13 +0100

    Added list filtering in findInModel

--- src/template/functions.js
+++ src/template/functions.js
@@ -11,22 +11,24 @@ module.exports = (nunjucksEnv, model, config) => {
 }
 
 // Find in the model every item corresponding to given filter following the format type{argumentKey: value}
-// ex: entity{name: user}
+// ex: entity{name: user, methods: {name: clear_basket}}
 function findInModel(request, model, config) {
     let sepatatorIndex = request.indexOf('{')
     let itemType = request.substr(0, sepatatorIndex)
     let itemFilters = yaml.safeLoad(request.substr(sepatatorIndex))
-    let result = model.filter(i => i.type == itemType).map(i => i.arguments)
 
     function filterItems(modelSubTree, filters) {
         let res = true
+
         for (let i in filters) {
             let subTree = modelSubTree
             i.split('.').forEach(j => subTree = subTree[j])
 
-            if (typeof subTree == 'string') res = res && subTree == filters[i]
+            if (typeof subTree == 'string') res = res && subTree === filters[i]
+            else if (Array.isArray(modelSubTree)) res = res && modelSubTree.some(i => filterItems(i, filters))
             else res = res && filterItems(subTree, filters[i])
         }
+
         return res
     }
 
